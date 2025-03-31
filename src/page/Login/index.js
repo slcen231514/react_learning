30a069edc60edc5f5685857ba2f4cc80eacd0b74
5612389 <2315144788@qq.com>
2025-03-31 19:59:42 +0800

    login part

--- src/page/Login/index.js
+++ src/page/Login/index.js
@@ -1,16 +1,29 @@
-import { Link, useNavigate } from "react-router-dom"
+import './index.scss'
+import { Card, Form, Input, Button } from 'antd'
+import logo from '@/assets/logo.png'
 
 const Login = () => {
-    const navigate = useNavigate()
-    return (
-        <div>
-            Login
-            <Link to="/article">跳转到文章页</Link>
-            <button onClick={() => navigate('/article')}>跳转到文章页</button>
-            <button onClick={() => navigate('/article?id=1001&name=jack')}>searchParams传参</button>
-            <button onClick={() => navigate('/article/1001/jack')}>params传参</button>
-        </div>
-    )
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form>
+          <Form.Item>
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item>
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
 }
 
 export default Login
