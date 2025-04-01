79ca9b744a075e96db650c62b6eea601f5b5c161
5612389 <2315144788@qq.com>
2025-04-01 14:19:30 +0800

    登录-表单校验 数据收集

--- src/page/Login/index.js
+++ src/page/Login/index.js
@@ -8,11 +8,32 @@ const Login = () => {
       <Card className="login-container">
         <img className="login-logo" src={logo} alt="" />
         {/* 登录表单 */}
-        <Form>
-          <Form.Item>
+        <Form validateTrigger="onChange">
+          <Form.Item 
+            name="mobile"
+            // 多条校验逻辑 按顺序校验
+            rules={[
+              {
+              required: true,
+              message: '请输入手机号！'
+              },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: '请输入正确的手机号格式'
+              },
+            ]}
+          >
             <Input size="large" placeholder="请输入手机号" />
           </Form.Item>
-          <Form.Item>
+          <Form.Item 
+            name="code"
+            rules={[
+              {
+              required: true,
+              message: '请输入验证码！'
+              },
+            ]}
+          >
             <Input size="large" placeholder="请输入验证码" />
           </Form.Item>
           <Form.Item>
@@ -26,4 +47,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Login 
