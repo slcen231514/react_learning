5aa6f56c5b6f1af957d5db2ab1bc20f79e5559e5
5612389 <2315144788@qq.com>
2025-04-04 14:48:37 +0800

    文章列表-列表筛选

--- src/page/Article/index.js
+++ src/page/Article/index.js
@@ -94,17 +94,42 @@ const Article = () => {
     }
   ]
 
+  // 筛选功能
+  // 1.准备参数
+  const [reqData, setReqData] = useState({
+    status: '',
+    channel_id: '',
+    begin_pubdate: '',
+    end_pubdate: '',
+    page: 1,
+    per_page: 4,
+  })
+
   // 获取文章列表
   const [list, setList] = useState([])
   const [count , setCount] = useState(0)
   useEffect(() => {
     async function getList() {
-        const res = await getArticleListAPI()
+        const res = await getArticleListAPI(reqData)
         setList(res.data.results)
         setCount(res.data.total_count)
     }
     getList()
-  }, [])
+  }, [reqData])
+
+  // 2.获取当前的筛选数据
+  const onFinish = (formValue) => {
+    // 3.把表单收集到的数据放到参数中(不可变的方式)
+    setReqData({
+        ...reqData,
+        channel_id: formValue.channel_id,
+        status: formValue.status,
+        begin_pubdate: formValue.date[0].format('YYYY-MM-DD'),
+        end_pubdate: formValue.date[1].format('YYYY-MM-DD'),
+    })
+    // 4.重新拉取文章列表 + 渲染table逻辑重复的 - 复用
+    // reqData依赖项发生变化 重复执行副作用函数
+  }
 
   return (
     <div>
@@ -117,11 +142,11 @@ const Article = () => {
         }
         style={{ marginBottom: 20 }}
       >
-        <Form initialValues={{ status: '' }}>
+        <Form initialValues={{ status: '' }} onFinish={onFinish}>
           <Form.Item label="状态" name="status">
             <Radio.Group>
               <Radio value={''}>全部</Radio>
-              <Radio value={0}>草稿</Radio>
+              <Radio value={1}>待审核</Radio>
               <Radio value={2}>审核通过</Radio>
             </Radio.Group>
           </Form.Item>
